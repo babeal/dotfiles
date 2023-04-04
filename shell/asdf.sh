8359264d74aeaa69a3f68aae021a2a362935327a
Brandt Beal <babeal@outlook.com>
2023-04-04 17:05:37 -0500

    feat: fix asdf loading on bash

--- shell/asdf.sh
+++ shell/asdf.sh
@@ -1,5 +1,15 @@
 
 # ASDF Package Manager
-# supposefly --prefix asdf requires ruby which makes it slow where --prefix is fast
-local pkg_root=$(brew --prefix)
-[[ -s "$pkg_root/opt/asdf/libexec/asdf.sh" ]] && source "$pkg_root/opt/asdf/libexec/asdf.sh"
+
+if command -v brew &>/dev/null; then
+    # supposefly --prefix asdf requires ruby which makes it slow where --prefix is fast
+    local pkg_root=$(brew --prefix)
+    [[ -s "$pkg_root/opt/asdf/libexec/asdf.sh" ]] && source "$pkg_root/opt/asdf/libexec/asdf.sh"
+elif [[ -s ~/.asdf/asdf.sh]]; then
+    source ~/.asdf/asdf.sh
+    if [[ -n ${BASH} ]]; then
+        source ~/.asdf/completions/asdf.bash
+    elif [[ -n ${ZSH_NAME} ]]; then
+        # add directory to fpath if needed
+    fi
+fi
