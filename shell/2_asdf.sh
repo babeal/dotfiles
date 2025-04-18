b1dcf61bea661c0be42facc72fa3edc735695600
Brandt Beal <babeal@outlook.com>
2025-04-18 13:20:08 -0500

    feat: update to asdf v16

--- shell/2_asdf.sh
+++ shell/2_asdf.sh
@@ -1,16 +1,17 @@
 
 # ASDF Package Manager
-
-if command -v brew &>/dev/null; then
-    # supposefly --prefix asdf requires ruby which makes it slow where --prefix is fast
-    local pkg_root=$(brew --prefix)
-    [[ -s "$pkg_root/opt/asdf/libexec/asdf.sh" ]] && source "$pkg_root/opt/asdf/libexec/asdf.sh"
-elif [[ -s "~/.asdf/asdf.sh" ]]; then
-    source ~/.asdf/asdf.sh
-    if [[ -n ${BASH} ]]; then
-        source ~/.asdf/completions/asdf.bash
-    elif [[ -n ${ZSH_NAME} ]]; then
-        # add directory to fpath if needed
-        :
-    fi
-fi
+export ASDF_DATA_DIR="$HOME/.asdf"
+export PATH="$ASDF_DATA_DIR/shims:$PATH"
+# if command -v brew &>/dev/null; then
+#     # supposefly --prefix asdf requires ruby which makes it slow where --prefix is fast
+#     local pkg_root=$(brew --prefix)
+#     [[ -s "$pkg_root/opt/asdf/libexec/asdf.sh" ]] && source "$pkg_root/opt/asdf/libexec/asdf.sh"
+# elif [[ -s "~/.asdf/asdf.sh" ]]; then
+#     source ~/.asdf/asdf.sh
+#     if [[ -n ${BASH} ]]; then
+#         source ~/.asdf/completions/asdf.bash
+#     elif [[ -n ${ZSH_NAME} ]]; then
+#         # add directory to fpath if needed
+#         :
+#     fi
+# fi
---
+++ shell/gh.sh
@@ -0,0 +1,4 @@
+
+if command -v eza &>/dev/null; then
+    eval "$(gh copilot alias -- zsh)"
+fi
